fd6b66b0cc09db18288fd3477903dde44dd86484
andreas <caternberg@gmx.de>
2023-10-05 17:31:59 +0200

    getops, option for ebs snapshot or direct sync

--- README.md
+++ README.md
@@ -4,9 +4,28 @@ This repo is about to upgrade a CB CI EBS Controller to EFS Controller
 * Background see https://docs.cloudbees.com/docs/cloudbees-ci/latest/ha-install-guide/install-ha-on-platforms#_migrate_an_existing_managed_controller_controller_to_high_availability_ha
 
 # Get started
-* set the variable DOMAIN in `upgradeController.sh` to your EBS controller name and run the script
-* Once the script is done, got to CJOC UI and delete the Controller. 
-* * The pvc with JENKINS_HOME (which have been updated in the first step) will retain (see retain policy)
+
+## Upgrade Controller from EBS to EFS including EBS Snapshot volume
+this will reduce the downtime of a Controller, useful for huge JENKINS_HOME volume sizes when Controller availability is important
+> ./upgradeController.sh -c YOUR_CONTROLLER_NAME  -e 1
+
+Example
+> ./upgradeController.sh -c team-ebs-controller1  -e 1
+
+## Upgrade Controller from EBS to EFS directly from Controller PV volume (No EBS Snapshot)
+This will create a EFS PV and sync JENKINS_HOME data direcly from the Controller PV volume
+Before the sync the statefulset of the Controller will be scaled to zero. 
+This might increase the downtime ofthe Controller
+
+> > ./upgradeController.sh -c YOUR_CONTROLLER_NAME  -e 0
+
+Example
+> ./upgradeController.sh -c team-ebs-controller1  -e 0
+
+
+
+* Once the script is done, got to CJOC UI and delete the Controller.
+ * * The original volume with JENKINS_HOME (EBS,RWO) will retain (see retain policy)
 * Then recreate the Controller in CJOC with the same name. Ensure that the storage class in CJOC Controller config screen is pointing to an efs storage class before you start the new Controller
 * CB CasC might help to avoid the manual steps 
 * Once the Controller is up again it should have the data from the previous EBS JENKINS_HOME volume
--- upgradeController.sh
+++ upgradeController.sh
@@ -3,17 +3,24 @@
 set -euo pipefail
 
 #The source Controller name
-export DOMAIN=controller-ebs
-export GENDIR=generated
+export CONTROLLER_NAME=${CONTROLLER_NAME:-"controller-ebs"}
+#By default EBS snapshots are disabled -> ß
+export WITH_EBS_SNAPSHOT=0
 
-mkdir -p $GENDIR
-rm -Rf $GENDIR/*
+show_help (){
+  echo "Usage: ${0##*/} -c <CONTROLLER_NAME> [OPTIONS]
+          -h                display this help and exit
+          -c controller     CONTROLLER_NAME
+          -e                Do EBS Snapshot and sync from snapshot volume
+        Example: ${0##*/} -c my-ebs-controller -e 1 # create an EBS snapshot volume (reduces the controller downtime)
+        Example: ${0##*/} -c my-ebs-controller -e 0 # synchronize directly from jenkins_home pv, (increases the controller downtime)
+     "
+}
+
 
 continueOrExit (){
-echo "Press 'y' to continue or 'n' to exit."
-  # Wait for the user to press a key
+  echo "Press 'y' to continue or 'n' to exit."
   read -s -n 1 key
-  # Check which key was pressed
   case $key in
       y|Y)
           echo "You pressed 'y'. Continuing..."
@@ -28,64 +35,95 @@ echo "Press 'y' to continue or 'n' to exit."
   esac
 }
 
-echo "Create EBS snapshot and volume for $DOMAIN"
-./scripts/createEBSSnapshotAndVolume.sh $DOMAIN
-continueOrExit
+
+if [[ ${#} -eq 0 ]]; then
+   show_help
+   exit 0
+fi
+
+
+OPTIND=1
+# Resetting OPTIND is necessary if getopts was used previously in the script.
+# It is a good idea to make OPTIND local if you process options in a function.
+while getopts :h:e:c: opt; do
+    case $opt in
+        h)
+            show_help
+            exit 0
+            ;;
+        c)  CONTROLLER_NAME=$OPTARG
+            ;;
+        e)  WITH_EBS_SNAPSHOT=1
+            ;;
+        *)
+            show_help >&2
+            exit 1
+            ;;
+    esac
+done
+shift "$((OPTIND-1))"   # Discard the options and sentinel --
+
+echo "CONTROLLER_NAME: $CONTROLLER_NAME"
+echo "WITH_EBS_SNAPSHOT: $WITH_EBS_SNAPSHOT"
+
+export GENDIR=generated
+mkdir -p $GENDIR
+rm -Rf $GENDIR/*
 
 echo "Backup existing Controller resources"
-kubectl get statefulset  -l tenant=$DOMAIN -o yaml > $GENDIR/$DOMAIN-statefulset-source.yaml
-kubectl get service      -l tenant=$DOMAIN -o yaml > $GENDIR/$DOMAIN-service-source.yaml
-kubectl get ing          -l tenant=$DOMAIN -o yaml > $GENDIR/$DOMAIN-ing-source.yaml
-kubectl get pvc          -l tenant=$DOMAIN -o yaml > $GENDIR/$DOMAIN-pvc-source.yaml
-kubectl get pv $(kubectl get "pvc/jenkins-home-${DOMAIN}-0" -o go-template={{.spec.volumeName}}) -o yaml   > $GENDIR/$DOMAIN-pv-source.yaml
 continueOrExit
+kubectl get statefulset  -l tenant=$CONTROLLER_NAME -o yaml > $GENDIR/$CONTROLLER_NAME-statefulset-source.yaml
+kubectl get service      -l tenant=$CONTROLLER_NAME -o yaml > $GENDIR/$CONTROLLER_NAME-service-source.yaml
+kubectl get ing          -l tenant=$CONTROLLER_NAME -o yaml > $GENDIR/$CONTROLLER_NAME-ing-source.yaml
+kubectl get pvc          -l tenant=$CONTROLLER_NAME -o yaml > $GENDIR/$CONTROLLER_NAME-pvc-source.yaml
+kubectl get pv $(kubectl get "pvc/jenkins-home-${CONTROLLER_NAME}-0" -o go-template={{.spec.volumeName}}) -o yaml   > $GENDIR/$CONTROLLER_NAME-pv-source.yaml
 
-export VOLUMEID=$(cat $GENDIR/ebs-snapshot_volume.json |jq -r  '.VolumeId')
-echo "Create a new PV from the EBS snapshot volume with volume_id $VOLUMEID "
-envsubst < yaml/pv-backup-jenkins-home-0.yaml  |kubectl apply -f -
-continueOrExit
+if [ $WITH_EBS_SNAPSHOT = 1 ]; then
+  echo "Create EBS snapshot and volume for $CONTROLLER_NAME"
+  continueOrExit
+  ./scripts/createEBSSnapshotAndVolume.sh $CONTROLLER_NAME
 
-echo "Create a PVC that clains the backup PV"
-#kubectl get "pvc/jenkins-home-${DOMAIN}-0" -o yaml > yaml/pvc-backup-jenkins-home-${DOMAIN}-0.yaml
-envsubst < yaml/pvc-backup-jenkins-home-0.yaml  |kubectl apply -f -
-continueOrExit
+  export VOLUMEID=$(cat $GENDIR/ebs-snapshot_volume.json |jq -r  '.VolumeId')
+  echo "Create a new PV from the EBS snapshot volume with volume_id $VOLUMEID "
+  envsubst < yaml/pv-backup-jenkins-home-0.yaml  |kubectl apply -f -
 
-#Alocate a backup job that uses the backup PVC (the wolume made from EBS snapshot))
-envsubst < yaml/allocate-backup-jenkins-home-0.yaml |kubectl apply -f -
-continueOrExit
-kubectl wait --for=condition=complete job/allocate-backup-jenkins-home-${DOMAIN}-0
-continueOrExit
-kubectl delete job/allocate-backup-jenkins-home-${DOMAIN}-0
-continueOrExit
+  echo "Create a PVC that clains the backup PV"
+  #kubectl get "pvc/jenkins-home-${CONTROLLER_NAME}-0" -o yaml > yaml/pvc-backup-jenkins-home-${CONTROLLER_NAME}-0.yaml
+  envsubst < yaml/pvc-backup-jenkins-home-0.yaml  |kubectl apply -f -
 
-#Create new volume with ReadWriteMany RWX
+  echo "Allocate a backup job that uses the backup PVC (the volume that was made from EBS snapshot))"
+  envsubst < yaml/allocate-backup-jenkins-home-0.yaml |kubectl apply -f -
+  kubectl wait --for=condition=complete job/allocate-backup-jenkins-home-${CONTROLLER_NAME}-0
+  kubectl delete job/allocate-backup-jenkins-home-${CONTROLLER_NAME}-0
+fi
+
+echo "Create new volume with ReadWriteMany RWX"
 envsubst < yaml/pvc-rwx-jenkins-home-0.yaml |kubectl apply -f -
-continueOrExit
 
-#Test if we can claim the RWX volume
+echo "Test if we can claim the RWX volume"
 envsubst < yaml/allocate-rwx-jenkins-home-0.yaml |kubectl apply -f -
-continueOrExit
-kubectl wait --for=condition=complete job/allocate-rwx-jenkins-home-${DOMAIN}-0
-continueOrExit
-kubectl delete job/allocate-rwx-jenkins-home-${DOMAIN}-0
+kubectl wait --for=condition=complete job/allocate-rwx-jenkins-home-${CONTROLLER_NAME}-0
+kubectl delete job/allocate-rwx-jenkins-home-${CONTROLLER_NAME}-0
 
-#The following two lines would sync directly from the existing jenkins-home-${DOMAIN}-0. If so a scale down is required before
-#kubectl scale statefulsets/$DOMAIN --replicas=0
-#./pvc-sync.sh jenkins-home-${DOMAIN}-0 pvc-rwx-jenkins-home-${DOMAIN}-0
+if [ $WITH_EBS_SNAPSHOT = 1 ]; then
+    echo "sync JENKINS_HOME data from backup-jenkins-home-${CONTROLLER_NAME}-0 to the new RWX volume pvc-rwx-jenkins-home-${CONTROLLER_NAME}-0"
+    #continueOrExit
+    ./scripts/pvc-sync.sh backup-jenkins-home-${CONTROLLER_NAME}-0 pvc-rwx-jenkins-home-${CONTROLLER_NAME}-0
+    echo "In case of EBS snapshot PV we need to scale down the Controller now"
+    #continueOrExit
+    kubectl scale statefulsets/$CONTROLLER_NAME --replicas=0
+else
+    echo "Scale down replicaset to zero and sync driectly from jenkins-home-${CONTROLLER_NAME}-0"
+    #continueOrExit
+    kubectl scale statefulsets/$CONTROLLER_NAME --replicas=0
+    ./scripts/pvc-sync.sh jenkins-home-${CONTROLLER_NAME}-0 pvc-rwx-jenkins-home-${CONTROLLER_NAME}-0
+fi
 
-echo "sync JENKINS_HOME data from backup-jenkins-home-${DOMAIN}-0 to the new RWX volume pvc-rwx-jenkins-home-${DOMAIN}-0"
-./scripts/pvc-sync.sh backup-jenkins-home-${DOMAIN}-0 pvc-rwx-jenkins-home-${DOMAIN}-0
-continueOrExit
-
-#In case of EBS snapshot PV we need to scale down the Controller now
-kubectl scale statefulsets/$DOMAIN --replicas=0
-continueOrExit
-
-#Replace the claimref and pv with the new volume (EFS/RWX)
-./scripts/rename_pvc.sh pvc-rwx-jenkins-home-${DOMAIN}-0 jenkins-home-${DOMAIN}-0
+echo "Replace the claimref and pv with the new volume (EFS/RWX), then rename pvc-rwx-jenkins-home-${CONTROLLER_NAME}-0 to jenkins-home-${CONTROLLER_NAME}-0"
+#continueOrExit
+./scripts/rename_pvc.sh pvc-rwx-jenkins-home-${CONTROLLER_NAME}-0 jenkins-home-${CONTROLLER_NAME}-0
 
 echo "Next: delete the Controller in CJOC and recreate it with the same name- Ensure efs-sc is applied in provisioning  config"
 echo "for example you can use CasC: see  ./scripts/createController/createManagedController.sh"
-echo "Example:  cd scripts/createController/ && ./createManagedController.sh ${DOMAIN} efs-sc"
+echo "Example:  cd scripts/createController/ && ./createManagedController.sh ${CONTROLLER_NAME} efs-sc"
 echo "Then enable HA on the controller"
-
