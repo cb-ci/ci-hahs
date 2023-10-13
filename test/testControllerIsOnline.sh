9c054e37b7ea49f0141931454c5fae383f02c54f
andreas <caternberg@gmx.de>
2023-10-13 17:30:42 +0200

    update

--- test/testControllerIsOnline.sh
+++ test/testControllerIsOnline.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-echo "Usage for time logging:$0 |while IFS= read -r line; do printf '+%s %s\n' "$(date)" "$line"; done| tee logger.log"
+echo "Usage for time logging:$0 |while IFS= read -r line; do printf '%s %s\n' "$(date)" "$line"; done| tee logger.log"
 #CONTROLLERNAME
 CONTROLLER=ha #ADJUST ME
 #CB BASE URL
@@ -9,19 +9,20 @@ CONTROLLER_URL=$CB_URL/$CONTROLLER
 #kubectl get deployments
 checkOnline() {
   #kubectl rollout restart deployment $CONTROLLER
-  RESPONSEHEADERS=headers1
+  RESPONSEHEADERS=headers-controller-online
   while [ -n "$(curl -s -IL ${CONTROLLER_URL}/login)" ]
   do
     curl  -IL -o ${RESPONSEHEADERS} ${CONTROLLER_URL}/login
     #cat $RESPONSEHEADERS
-   if [ -n "$(cat $RESPONSEHEADERS |grep -E 'HTTP/2 201|HTTP/ 200')" ]
+    if [ -n "$(cat $RESPONSEHEADERS |grep -E 'HTTP/2 201|HTTP/ 200')" ]
     then
       echo "${CONTROLLER_URL} is offline and not responding"
     fi
     echo "${CONTROLLER_URL} is online and responding"
     REPLICA="$(cat $RESPONSEHEADERS     |grep -E "x-jenkins-replica-host.*"    |  awk '{print $2}')"
     REPLICA_IP="$(cat $RESPONSEHEADERS  |grep -E "x-jenkins-replica-address.*" |  awk '{print $2}')"
-    echo "REPLICA:   $REPLICA"
+    echo "REPLICA:      $REPLICA"
+    echo "REPLICA_IP:   $REPLICA_IP"
     #kubectl get pod -l com.cloudbees.cje.tenant=$CONTROLLER
     #kubectl rollout status deployment $CONTROLLER
     sleep 3
