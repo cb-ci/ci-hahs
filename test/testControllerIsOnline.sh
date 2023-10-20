1fe7db3173a893ae3f10037052c96a53c161e6f8
andreas <caternberg@gmx.de>
2023-10-20 06:59:47 +0200

    source ./setenv.sh

--- test/testControllerIsOnline.sh
+++ test/testControllerIsOnline.sh
@@ -1,33 +1,33 @@
 #! /bin/bash
 
-echo "Usage for time logging:$0 |while IFS= read -r line; do printf '%s %s\n' "\$(date)" "\$line"; done| tee logger.log"
-#CONTROLLERNAME
-CONTROLLER=ha #ADJUST ME
+#echo "Usage for time logging:$0 |while IFS= read -r line; do printf '%s %s\n' "\$(date)" "\$line"; done| tee logger.log"
+
+source ./setenv.sh
+
 #CB BASE URL
-CB_URL=https://ci.acaternberg.pscbdemos.com # ADJUST ME
-CONTROLLER_URL=$CB_URL/$CONTROLLER
+CI_CONTROLLER_URL=$CI_BASE_URL/$CI_CONTROLLER
 LIVENESS_PROBE_URI=/whoAmI/api/json?tree=authenticated
 CONNECT_TIMEOUT=5
 #kubectl get deployments
 checkOnline() {
-  #kubectl rollout restart deployment $CONTROLLER
-  RESPONSEHEADERS=headers-controller-online
-  while [ -n "$(curl --connect-timeout $CONNECT_TIMEOUT  -s -IL ${CONTROLLER_URL}/$LIVENESS_PROBE_URI)" ]
+  #kubectl rollout restart deployment $CI_CONTROLLER
+  RESPONSEHEADERS=headers-CI_CONTROLLER-online
+  while [ -n "$(curl --connect-timeout $CONNECT_TIMEOUT  -s -IL ${CI_CONTROLLER_URL}/$LIVENESS_PROBE_URI)" ]
   do
     echo "##################################################"
-    curl --connect-timeout  $CONNECT_TIMEOUT  -s -IL -o ${RESPONSEHEADERS} ${CONTROLLER_URL}/$LIVENESS_PROBE_URI
+    curl --connect-timeout  $CONNECT_TIMEOUT  -s -IL -o ${RESPONSEHEADERS} ${CI_CONTROLLER_URL}/$LIVENESS_PROBE_URI
     #cat $RESPONSEHEADERS
     if [ -n "$(cat $RESPONSEHEADERS |grep -E 'HTTP/2 201|HTTP/ 200')" ]
     then
-      echo "${CONTROLLER_URL} is offline and not responding"
+      echo "${CI_CONTROLLER_URL} is offline and not responding"
     fi
-    echo "${CONTROLLER_URL} is online and responding"
+    echo "${CI_CONTROLLER_URL} is online and responding"
     REPLICA="$(cat $RESPONSEHEADERS     |grep -E "x-jenkins-replica-host.*"    |  awk '{print $2}')"
     REPLICA_IP="$(cat $RESPONSEHEADERS  |grep -E "x-jenkins-replica-address.*" |  awk '{print $2}')"
     echo "REPLICA:      $REPLICA"
     echo "REPLICA_IP:   $REPLICA_IP"
-    #kubectl get pod -l com.cloudbees.cje.tenant=$CONTROLLER
-    #kubectl rollout status deployment $CONTROLLER
+    #kubectl get pod -l com.cloudbees.cje.tenant=$CI_CONTROLLER
+    #kubectl rollout status deployment $CI_CONTROLLER
     sleep $CONNECT_TIMEOUT
   done
 }
