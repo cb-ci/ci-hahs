87478e9b62c9abfe913a74f6f7f367aaf079b5e1
andreas <caternberg@gmx.de>
2023-10-27 10:18:44 +0200

    update

--- test/pipelines/Jenkinsfile-K8s-Agent-loop-sleep.groovy
+++ test/pipelines/Jenkinsfile-K8s-Agent-loop-sleep.groovy
@@ -21,12 +21,33 @@ pipeline {
             steps {
                 //sleep time: 1, unit: 'HOURS'
                 //input id: 'Input', message: 'abort', ok: 'continue'
+
+
+                //This requires script approvals
+                /*
+                    method java.net.InetAddress getCanonicalHostName
+                    staticMethod java.net.InetAddress getLocalHost
+                 */
+                script {
+                    println("Running on Controller Host : " + InetAddress.localHost.canonicalHostName)
+                }
+                echo '''
+                    YOU CAN RUN NOW FOR TESTING:
+                    kubectl delete pod <ACTIVE_REPLICA_POD>
+                    
+                    YOU CAN WATCH THE REPLICA DETAILS:
+                    kubectl get rs
+                    kubectl get deployment
+                    kubectl top pods
+                                        
+                    THEN WATCH THE PIPELINE LOG TO VERIFY THE AGENT WAS RECONNECTED TO THE OTHER REPLICA     
+                '''
+
                 sh '''
+                    set +x
                     while true
                     do
-                     	echo "#######################################"
-                    	echo "Running on Agent-Pod: $(hostname)"
-                    	printf '%s %s\n' "$(date)"
+                    	printf '%s %s\n' "$(date) Running on Agent-Pod: $(hostname)"
                     	sleep 2
                     done
                 '''
